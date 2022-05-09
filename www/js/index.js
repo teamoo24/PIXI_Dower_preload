e44b5b25da74c9a5e2dede34e3daf751b5736346
Han Kyungsoo <dripcoke@gmail.com>
2022-05-10 00:21:24 +0900

    Preloading Assets

--- www/js/index.js
+++ www/js/index.js
@@ -33,4 +33,37 @@ window.onload = function () {
         backgroundColor: 0xAAAAAA
     });
     document.body.appendChild(app.view);
+    // preload assets
+    app.loader.baseUrl = "image";
+    app.loader
+        .add("sprite01", "bloat01.png")
+        .add("sprite02", "bloat02.png")
+        .add("sprite03", "bloat03.png")
+        .add("sprite04", "bloat04.png")
+        .add("sprite05", "bloat05.png")
+        .add("sprite06", "bloat06.png")
+        .add("sprite07", "bloat07.png")
+        .add("sprite08", "bloat08.png")
+        .add("sprite09", "bloat09.png")
+        .add("sprite10", "bloat10.png")
+        .add("player", "player.png");
+    app.loader.onProgress.add(showProgress);
+    app.loader.onComplete.add(doneLoading);
+    app.loader.onError.add(reportingError);
+    app.loader.load();
+};
+let showProgress = (e) => {
+    console.log(e.progress);
+};
+let reportingError = (e) => {
+    console.log("ERROR: " + e.message);
+};
+let doneLoading = (e) => {
+    console.log("DONE LOADING!");
+    if (app.loader.resources.player.texture != undefined)
+        player = PIXI.Sprite.from(app.loader.resources.player.texture);
+    player.x = app.view.width / 2;
+    player.y = app.view.height / 2;
+    player.anchor.set(0.5);
+    app.stage.addChild(player);
 };
--- www/ts/index.ts
+++ www/ts/index.ts
@@ -1,7 +1,7 @@
 import * as PIXI from "pixi.js"
 
-let app;
-let player;
+let app:PIXI.Application;
+let player:PIXI.Sprite;
 
 window.onload = function () {
     app = new PIXI.Application(
@@ -12,4 +12,44 @@ window.onload = function () {
         }
     )
     document.body.appendChild(app.view);
+
+    // preload assets
+    app.loader.baseUrl = "image";
+    app.loader
+        .add("sprite01","bloat01.png")
+        .add("sprite02","bloat02.png")
+        .add("sprite03","bloat03.png")
+        .add("sprite04","bloat04.png")
+        .add("sprite05","bloat05.png")
+        .add("sprite06","bloat06.png")
+        .add("sprite07","bloat07.png")
+        .add("sprite08","bloat08.png")
+        .add("sprite09","bloat09.png")
+        .add("sprite10","bloat10.png")
+        .add("player","player.png")
+
+    app.loader.onProgress.add(showProgress);
+    app.loader.onComplete.add(doneLoading)
+    app.loader.onError.add(reportingError)
+
+    app.loader.load();
+}
+
+let showProgress = (e: { progress: any; }) => {
+    console.log(e.progress)
+}
+
+let reportingError = (e: { message: string; }) => {
+    console.log("ERROR: " + e.message)
+}
+
+let doneLoading = (e: any) => {
+    console.log("DONE LOADING!")
+    
+    if(app.loader.resources.player.texture != undefined)
+    player = PIXI.Sprite.from(app.loader.resources.player.texture);
+    player.x = app.view.width /2;
+    player.y = app.view.height /2;
+    player.anchor.set(0.5);
+    app.stage.addChild(player)
 }
