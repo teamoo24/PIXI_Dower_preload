a17d360ae80da17d160e1b8c6efa282119fb6366
han-bbo <78396716+han-bbo@users.noreply.github.com>
2022-05-12 11:41:02 +0900

    Using CSS Fonts

--- www/src/emitted/using_css_font.js
+++ www/src/emitted/using_css_font.js
@@ -23,19 +23,21 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.using_css_font = void 0;
 const PIXI = __importStar(require("pixi.js"));
-//Basic Bulletsのモジュール
-const using_css_font_1 = require("./using_css_font");
-window.onload = function () {
-    let app = new PIXI.Application({
-        width: window.innerWidth - 4,
-        height: window.innerHeight - 4,
-        backgroundColor: 0xAAAAAA
-    });
-    // @ts-ignore
-    document.querySelector("#gameDiv").appendChild(app.view);
-    // let preloader_source = new preloader(app,player).preloader_source();
-    // new Basic_Bullets(app);
-    ///new Basic_collision(app);
-    new using_css_font_1.using_css_font(app);
-};
+class using_css_font {
+    constructor(app) {
+        this.app = app;
+        let text1 = new PIXI.Text("Welcome To Your Doom!");
+        text1.x = app.view.width / 2;
+        text1.y = app.view.height / 2;
+        text1.anchor.set(0.5);
+        text1.style = new PIXI.TextStyle({
+            fill: 0xff0000,
+            fontSize: 40,
+            fontFamily: "Arcade",
+        });
+        app.stage.addChild(text1);
+    }
+}
+exports.using_css_font = using_css_font;
--- www/src/index.ts
+++ www/src/index.ts
@@ -5,6 +5,8 @@ import {preloader} from './preloader.js'
 import {Basic_Bullets} from './Basic_Bullets.js'
 //Basic Bulletsのモジュール
 import {Basic_collision} from './Basic_collision.js'
+//Basic Bulletsのモジュール
+import {using_css_font} from './using_css_font'
 
 window.onload = function () {
     let app:PIXI.Application = new PIXI.Application(
@@ -20,6 +22,7 @@ window.onload = function () {
 
     // let preloader_source = new preloader(app,player).preloader_source();
     // new Basic_Bullets(app);
-    new Basic_collision(app);
+    ///new Basic_collision(app);
+    new using_css_font(app)
 }
 
